9c8c15e21ec5c01e3c218c9bafe780065870c5a8
Jimmy Ho <ho@jimmyho.com>
2015-12-18 01:35:09 +0000

    Rest_framework setting

--- jwtAPI/settings.py
+++ jwtAPI/settings.py
@@ -72,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jwtAPI.wsgi.application'
 
-AUTH_USER_MODEL = 'authentication.User'
 
 
 # Database
@@ -123,3 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'authentication.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
